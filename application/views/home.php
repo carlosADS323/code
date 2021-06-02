2d8dbe5b2a8b5679ec3738c4e33674d947e7e421
Jáder Oliveiraa <cadulira2000@gmail.com>
2021-06-01 21:22:04 -0300

    criação do controller  e view  Home

--- application/views/home.php
+++ application/views/home.php
@@ -6,6 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <link href="../../public/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <meta charset="UTF-8">
         <title></title>
     </head>
