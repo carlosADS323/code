2ccb0c5b0e30acda149b10225e8cf5639b9fd171
carlosADS323 <64919930+carlosADS323@users.noreply.github.com>
2021-06-17 11:09:32 -0300

    Commit com front

---
+++ application/views/home/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    </head>
+    <body>
+        </br></br></br></br></br></br>
+        
+        <h1>TODO O CONTEUDO DESSA INDEX "HOME" FICA AQUI DENTRO DE BODY</h1>
+        
+        </br></br></br></br></br>
+    </body>
+
+
+</html>
+
